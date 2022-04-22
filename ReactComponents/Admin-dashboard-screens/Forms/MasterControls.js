02448db4897d7e275287ea64f678d7208919b1b6
zeeshan-thedeveloper <zeeshanahmedd0010@gmail.com>
2022-04-23 00:02:30 +0500

    completed ui of settings

--- ReactComponents/Admin-dashboard-screens/Forms/MasterControls.js
+++ ReactComponents/Admin-dashboard-screens/Forms/MasterControls.js
@@ -1,4 +1,101 @@
-const MasterControls = ()=>{
-    return <div>MasterControls</div>
-}
+import {
+  Container,
+  Divider,
+  FormControlLabel,
+  Grid,
+  Switch,
+} from "@mui/material";
+import Heading from "../../../Support/Heading";
+
+const MasterControls = () => {
+  return (
+    <Container style={{ paddingTop: "2%" }}>
+      <div style={{backgroundColor:"red",paddingLeft: "1%"}}>
+        <Heading text="Master Controls" fontSize="1.5rem" />
+        <Divider />
+      </div>
+      <div style={{ marginTop: "2%" }}>
+        <Grid container >
+         
+          <Grid
+            item
+            xs={3}
+            style={{
+              border: "1px solid #7ea69f",
+              padding: "2%",
+              textAlign: "center",
+              marginLeft: "1%"
+            }}
+          >
+            <Heading
+              text="Terminate All Host Access Urls"
+              fontSize="1rem"
+            ></Heading>
+            <Divider />
+            <div>
+              <FormControlLabel
+                value="end"
+                control={<Switch color="secondary" defaultChecked />}
+                // label="Status"
+                // labelPlacement="Status"
+              />
+            </div>
+          </Grid>
+          <Grid item xs={1}></Grid>
+
+          <Grid
+            item
+            xs={3}
+            style={{
+              border: "1px solid #7ea69f",
+              padding: "2%",
+              textAlign: "center",
+              paddingLeft: "1%"
+            }}
+          >
+            <Heading
+              text="Terminate All Remote database Access Urls"
+              fontSize="1rem"
+            ></Heading>
+            <Divider />
+            <div>
+              <FormControlLabel
+                value="end"
+                control={<Switch color="secondary" defaultChecked />}
+                // label="Status"
+                // labelPlacement="Status"
+              />
+            </div>
+          </Grid>
+
+          <Grid item xs={1}></Grid>
+          <Grid
+            item
+            xs={3}
+            style={{
+              border: "1px solid #7ea69f",
+              padding: "2%",
+              textAlign: "center",
+              paddingLeft: "1%"
+            }}
+          >
+            <Heading
+              text="Desolve All consumer tokens"
+              fontSize="1rem"
+            ></Heading>
+            <Divider />
+            <div>
+              <FormControlLabel
+                value="end"
+                control={<Switch color="secondary" defaultChecked />}
+                // label="Status"
+                // labelPlacement="Status"
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
+};
 export default MasterControls;
