232b43550a0abc9e372155b42f39198d0cacebd5
zeeshan-thedeveloper <zeeshanahmedd0010@gmail.com>
2022-08-01 15:21:10 +0500

    update the server and front end urls

--- request-manager/urls.js
+++ request-manager/urls.js
@@ -2,7 +2,7 @@
 const FRONT_END_BASE_URL="https://central-api-layer-73ctvrbrm-zeeshan-thedeveloper.vercel.app";
 const BACK_END_BASE_URL="https://central-api-ldh.herokuapp.com";
 
-
+ 
 // const FRONT_END_BASE_URL="http://localhost:3000";
 // const BACK_END_BASE_URL="http://localhost:3003";
 
