3b688fd6b674915f9a8795bffc9c17896856d87b
Zeeshan <109207162+zeeshan-edgevideo@users.noreply.github.com>
2022-09-17 12:11:10 +0500

    merged final-touchup-v-2

--- request-manager/urls.js
+++ request-manager/urls.js
@@ -5,7 +5,7 @@ const BACK_END_BASE_URL="https://central-api-ldh.herokuapp.com";
 
 // const FRONT_END_BASE_URL="http://localhost:3000";
 // const BACK_END_BASE_URL="http://localhost:3003";
-
+ 
 module.exports={
     FRONT_END_BASE_URL,
     BACK_END_BASE_URL
