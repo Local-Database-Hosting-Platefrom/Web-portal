9b48adfd0aef657f386ade75754dbc03610c943d
Zeeshan <zeeshanahmedd0010@gmail.com>
2022-09-18 16:56:22 +0500

    updaed urls to download desktop application

--- pages/Downloads/index.js
+++ pages/Downloads/index.js
@@ -5,14 +5,18 @@ import Heading from "../../Support/Heading";
 import { Button, Card } from "antd";
 import React from "react";
 import { Avatar } from "antd";
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 const { Meta } = Card;
-import 'antd/dist/antd.css'; 
+import "antd/dist/antd.css";
 import FooterWraper from "../../ReactComponents/HomePage-Footer/FooterWraper";
 const useStyles = makeStyles({
   root: {
     marginTop: "5%",
-    paddingBottom:"5%"
+    paddingBottom: "5%",
   },
 });
 
@@ -21,58 +25,53 @@ const Index = () => {
   return (
     <div className={classes.root}>
       <div style={{ marginLeft: "10%", marginTop: "5rem" }}>
-        <Heading text={"Desktop Application Setup"} fontSize="2rem" fontWeight={"bold"} />
+        <Heading
+          text={"Desktop Application Setup"}
+          fontSize="2rem"
+          fontWeight={"bold"}
+        />
       </div>
       <div style={{ marginTop: "4%" }}>
         <Grid container style={{ textAlign: "center" }}>
-          <Grid item xs={4}>
-            
-          </Grid>
+          <Grid item xs={4}></Grid>
           <Grid item xs={3}>
             <Card
               style={{
                 width: 300,
-                padding:"3rem"
+                padding: "3rem",
               }}
-              cover={
-                <img
-                  alt="example"
-                  src="/windows.png"
-                  width="250"
-                />
-              }
-              
+              cover={<img alt="example" src="/windows.png" width="250" />}
             >
-            <div>
-                <div style={{fontSize:"1rem",fontWeight:"bold"}}>
-                    Windows version   
+              <div>
+                <div style={{ fontSize: "1rem", fontWeight: "bold" }}>
+                  Windows version
                 </div>
-                <div style={{marginTop:"3%"}}>
-            <Button
+                <div style={{ marginTop: "3%" }}>
+                  <Button
                     type="primary"
                     shape="round"
                     style={{ width: "70%" }}
                     size={"middle"}
                     onClick={() => {
-                    //   handleOkEvent(null);
-                    
-                  window.open("https://drive.google.com/file/d/1xI9EV5MdsLa8oRaie9wIRndMFOsEB7l0/view?usp=sharing");
+                      window.open(
+                        "https://drive.google.com/file/d/1qqDkglfd5ybSqpe-vpRjZq67sR8INJp8/view?usp=sharing"
+                      );
                     }}
                   >
                     Download
                   </Button>
                 </div>
-                <div style={{fontSize:"0.7rem",fontWeight:"bold"}}>
-                    File Size : ~ 191 MB   
+                <div style={{ fontSize: "0.7rem", fontWeight: "bold" }}>
+                  File Size : ~ 191 MB
                 </div>
-            </div>
+              </div>
             </Card>
           </Grid>
           <Grid item xs={3}>
-          <Card
+            <Card
               style={{
                 width: 300,
-                padding:"3rem"
+                padding: "3rem",
               }}
               cover={
                 <img
@@ -82,36 +81,35 @@ const Index = () => {
                   height="205"
                 />
               }
-               
             >
-            <div>
-                <div style={{fontSize:"1rem",fontWeight:"bold"}}>
-                    Linux version   
+              <div>
+                <div style={{ fontSize: "1rem", fontWeight: "bold" }}>
+                  Linux version
                 </div>
-                <div style={{marginTop:"3%"}}>
-            <Button
+                <div style={{ marginTop: "3%" }}>
+                  <Button
                     type="primary"
                     shape="round"
                     style={{ width: "70%" }}
                     size={"middle"}
                     onClick={() => {
-                    //   handleOkEvent(null);
-                    
-                  window.open("https://drive.google.com/file/d/1xI9EV5MdsLa8oRaie9wIRndMFOsEB7l0/view?usp=sharing");
+                      //   handleOkEvent(null);
+
+                      window.open(
+                        "https://drive.google.com/file/d/1qqDkglfd5ybSqpe-vpRjZq67sR8INJp8/view?usp=sharing"
+                      );
                     }}
                   >
                     Download
                   </Button>
                 </div>
-                <div style={{fontSize:"0.7rem",fontWeight:"bold"}}>
-                    File Size : ~ 191 MB   
+                <div style={{ fontSize: "0.7rem", fontWeight: "bold" }}>
+                  File Size : ~ 191 MB
                 </div>
-            </div>
+              </div>
             </Card>
           </Grid>
-          <Grid item xs={2}>
-            
-          </Grid>
+          <Grid item xs={2}></Grid>
         </Grid>
       </div>
     </div>
